4794b97d12afad1374df419f7d3b81b8b695c33e
EvgenMeshkin <programmbel@mail.ru>
2014-12-15 07:34:06 +0300

    coding orm

---
+++ WikipediaClient/app/src/main/java/com/example/evgen/apiclient/db/provider/WikiContentProvider.java
@@ -0,0 +1,7 @@
+package com.example.evgen.apiclient.db.provider;
+
+/**
+ * Created by evgen on 13.12.2014.
+ */
+public class WikiContentProvider {
+}
---
+++ WikipediaClient/app/src/main/java/com/example/evgen/apiclient/db/wikiorm/WikiOrmHelper.java
@@ -0,0 +1,7 @@
+package com.example.evgen.apiclient.db.wikiorm;
+
+/**
+ * Created by evgen on 13.12.2014.
+ */
+public class WikiOrmHelper {
+}
